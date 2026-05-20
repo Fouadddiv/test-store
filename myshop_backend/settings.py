a21314057a618528b01c6223c7841ec40be82fbf
Fouad Dev <fouad@example.com>
2026-05-20 21:21:45 +0100

    إصلاح نظام الملفات الثابتة والصور للإنتاج

--- myshop_backend/settings.py
+++ myshop_backend/settings.py
@@ -98,6 +98,10 @@ DATABASES = {
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
