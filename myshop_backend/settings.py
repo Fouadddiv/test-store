8257c7a54b32a1f4cec75802341c27e575528f39
Fouad Dev <fouad@example.com>
2026-05-21 17:40:38 +0100

    Enable debug for media

--- myshop_backend/settings.py
+++ myshop_backend/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3a!v551hn!#-7_pt5ds^furc*(wx8(psn!#+$e2h&dai49baw1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
-
+DEBUG = True
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
